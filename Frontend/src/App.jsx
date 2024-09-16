c73a8ee1ecdb85b973bf05754765623c28f54ea3
Ak4shKr <akashkmr7667@gmail.com>
2024-09-17 02:15:09 +0530

    routes changed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminSet from "./components/Admin/AdminSet";
-import UserUI from "./components/User/UserUI";
+import UserUI from "./components/User/Quiz";
+import CreateTest from "./components/Admin/CreateTest";
+import NavbarAdmin from "./components/Admin/NavbarAdmin";
+import UserInfo from "./components/User/UserInfo";
+import Quiz from "./components/User/Quiz";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/admin" element={<AdminSet />} />
-        <Route path="/user" element={<UserUI />} />
+        <Route path="/user" element={<UserInfo />} />
+        <Route path="/user/quiz/:testNumber" element={<Quiz />} />
+        <Route path="/admin/createtest" element={<CreateTest />} />
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </Router>
