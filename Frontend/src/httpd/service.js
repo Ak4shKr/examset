7eee10152a22a78feee5c41897081538d59fb368
Ak4shKr <akashkmr7667@gmail.com>
2024-09-18 19:33:57 +0530

    backed url config

--- Frontend/src/httpd/service.js
+++ Frontend/src/httpd/service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: "https://examset.onrender.com/api",
+  baseURL: "https://examset-1.onrender.com/api/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
