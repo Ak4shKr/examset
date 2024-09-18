70d19b57dfe15dc224b4847dec11dea9080168c7
Ak4shKr <akashkmr7667@gmail.com>
2024-09-18 20:08:28 +0530

    checking again againn

--- Frontend/src/httpd/service.js
+++ Frontend/src/httpd/service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const service = axios.create({
   baseURL: "https://examset-1.onrender.com/api",
-  withCredentials: true,
+  // withCredentials: true,
   // headers: {
   //   "Content-Type": "application/json",
   // },
